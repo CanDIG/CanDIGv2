fa00d9f72080ff8c2827f91a6f5554011dbbf22e
Shaikh Farhan Rashid <shaikh.rashid@sickkids.ca>
2018-11-04 15:44:14 -0500

    minio and mc shell working

--- mc-shell.sh
+++ mc-shell.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+set -xe
+
+if [ ! -d "$(pwd)/mnt/mc" ]; then 
+  mkdir -p $(pwd)/mnt/mc/data
+fi
+
+if [ ! -d "$(pwd)/etc/mc" ]; then 
+  mkdir -p $(pwd)/etc/mc/config
+  #cp $(pwd)/mc-sample.config.json $(pwd)/etc/minio/config.json
+fi
+
 docker run -it \
   --name=mc_shell \
-  -v $(pwd)/mnt/mc/config:/root/.mc \
-  -v $(pwd)/mnt/mc/data:/data
+  -v $(pwd)/mnt/mc/data:/data \
+  -v $(pwd)/etc/mc/config:/root/.mc \
   --entrypoint=/bin/sh \
   minio/mc
--- start-minio.sh
+++ start-minio.sh
@@ -2,9 +2,34 @@
 
 set -xe
 
+if [ ! -d "$(pwd)/mnt/minio1" ]; then
+  mkdir -p $(pwd)/mnt/minio1/data
+fi
+
+if [ ! -d "$(pwd)/etc/minio" ]; then
+  mkdir -p $(pwd)/etc/minio
+  #cp $(pwd)/minio-sample.config.json $(pwd)/etc/minio/config.json
+fi
+
+if [ ! -f "$(pwd)/credentials" ]; then
+  KEY=$(dd if=/dev/urandom bs=1 count=16 2>/dev/null | base64 | rev | cut -b 2- | rev)
+
+  cat <<EOF > $(pwd)/credentials
+admin
+$KEY
+EOF
+
+else
+  KEY=$(sed -n 2p credentials)
+fi
+
 docker run \
   -p 9000:9000 \
   --name minio1 \
+  -e MINIO_ACCESS_KEY="admin" \
+  -e MINIO_SECRET_KEY="$KEY" \
+  -e MINIO_REGION="candig" \
+  -e MINIO_DOMAIN="candig.com" \
   -v $(pwd)/mnt/minio1/data:/data \
-  -v $(pwd)/mnt/minio1/config:/root/.minio \
+  -v $(pwd)/etc/minio:/etc/minio \
   minio/minio server /data
