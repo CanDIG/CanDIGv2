9f7bcdb92917e86c0dc7d1fda17fc8e09a02a27e
Shaikh Farhan Rashid <shaikh.rashid@sickkids.ca>
2019-02-28 22:09:04 -0500

    images are added to candig repo

--- lib/htsnexus-server/demo.sh
+++ lib/htsnexus-server/demo.sh
@@ -2,20 +2,35 @@
 
 set -xe
 
-# Download an ENCODE ChIP-Seq BAM
-htsnexus ENCODE ENCFF904PIO > ENCFF904PIO.bam
+pushd /root
 
-# Slice a genomic range out of a Platinum Genomes BAM
-htsnexus -r chr12:111766922-111817529 platinum NA12878 > NA12878_ALDH2.bam
+## Download an ENCODE ChIP-Seq BAM
+#htsnexus ENCODE ENCFF904PIO > ENCFF904PIO.bam
 
-# Count reads on chr21 in a 1000 Genomes BAM
-htsnexus -r 21 1000genomes_low_coverage NA20276 | samtools view -c -
+## Slice a genomic range out of a Platinum Genomes BAM
+#htsnexus -r chr12:111766922-111817529 platinum NA12878 > NA12878_ALDH2.bam
 
-# id. with CRAM (samtools 1.2+ needed)
-htsnexus -r 21 1000genomes_low_coverage HG01102 CRAM | samtools view -c -
+## Count reads on chr21 in a 1000 Genomes BAM
+#htsnexus -r 21 1000genomes_low_coverage NA20276 | samtools view -c -
 
-# Stream reads from Heng Li's bamsvr and display as headered SAM
-htsnexus -r 11:10899000-10900000 lh3bamsvr EXA00001 | samtools view -h - | less -S
+## id. with CRAM (samtools 1.2+ needed)
+#htsnexus -r 21 1000genomes_low_coverage HG01102 CRAM | samtools view -c -
 
-# Slice a bgzipped VCF
-htsnexus -r 12:112204691-112247789 1000genomes 20130502_autosomes vcf | gzip -dc | grep rs671 | cut -f1-16
+## Stream reads from Heng Li's bamsvr and display as headered SAM
+#htsnexus -r 11:10899000-10900000 lh3bamsvr EXA00001 | samtools view -h - | less -S
+
+## Slice a bgzipped VCF
+#htsnexus -r 12:112204691-112247789 1000genomes 20130502_autosomes vcf | gzip -dc | grep rs671 | cut -f1-16
+
+curl -LO https://s3.amazonaws.com/1000genomes/phase3/data/NA20276/alignment/NA20276.mapped.ILLUMINA.bwa.ASW.low_coverage.20120522.bam
+
+curl -X POST -H 'Content-Type: application/json' \
+    --data '{"data_object":
+              {"id": "1000genomes-NA20276",
+               "name": "1000genomes_phase3_alignments_sequence_20150526_NA20276",
+               "checksums": [{"checksum": "41b47ce1cc21b558409c19b892e1c0d1", "type": "md5"}],
+               "urls": [{"url": "http://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/chr22.fa.gz"}],
+               "size": "12255678"}}' http://localhost:8080/ga4gh/dos/v1/dataobjects
+htsnexus_index_bam /root/server/test/test.sql 1000genomes_low_coverage NA20276 /root/NA20276.mapped.ILLUMINA.bwa.ASW.low_coverage.20120522.bam
+
+popd
--- lib/mc/mc-config.sh
+++ lib/mc/mc-config.sh
@@ -2,4 +2,10 @@
 
 set -xe
 
-mc config host add minio http://minio:9000 $(cat /run/secrets/access_key) $(cat /run/secrets/secret_key)
+mc config host add minio http://minio:9000 <(/run/secrets/access_key) <(/run/secrets/secret_key)
+
+wget https://s3.amazonaws.com/1000genomes/phase3/data/NA20276/alignment/NA20276.mapped.ILLUMINA.bwa.ASW.low_coverage.20120522.bam
+
+mc mb minio/candig/
+mc mb minio/candig/1000genomes/phase3/data/NA20276/alignment/
+mc cp NA20276.mapped.ILLUMINA.bwa.ASW.low_coverage.20120522.bam minio/candig/1000genomes/phase3/data/NA20276/alignment/
