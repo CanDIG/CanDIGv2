3a201a4f5e416ed77962e9666ceded5c9a17c179
Shaikh Farhan Rashid <shaikh.rashid@sickkids.ca>
2019-03-01 01:37:06 -0500

    demo of htsnexus-server with streaming alignment

--- lib/jupyter/demo/demo.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python2
-
-import htsget
-
-url = "http://htsnexus.rnd.dnanex.us/v1/reads/BroadHiSeqX_b37/NA12878"
-with open("NA12878_2.bam", "wb") as output:
-    htsget.get(url, output, reference_name="2", start=1000, end=20000)
