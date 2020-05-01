e049abb82913e9b98993eb1213dc4cd22383dcbb
Felipe <fcoralsasso@bcgsc.ca>
2020-05-01 16:55:06 -0700

    Added script for docker-free deployment

---
+++ docker_free_installation.sh
@@ -0,0 +1,20 @@
+git submodule update --init --recursive
+
+# initialize conda environment
+make init-conda
+
+source miniconda3/etc/profile.d/conda.sh
+
+conda activate candig
+
+# download minio server/client
+make bin-minio
+
+# generate secrets for minio server/client
+make minio-secrets
+
+# start minio server instance
+# start drs server instance
+# start htsget server instance
+#make minio-server & 
+make conda-drs-server & make conda-htsget-server & make conda-federation-service
