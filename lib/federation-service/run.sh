0f0d8bee53a4d0e5a0411b71885aa3d9df87fb7b
Felipe <fcoralsasso@bcgsc.ca>
2020-05-04 17:16:49 -0700

    Changed code to create a virtual env

--- lib/federation-service/run.sh
+++ lib/federation-service/run.sh
@@ -7,6 +7,9 @@ source .env
 [ -z $CONDA_DEFAULT_ENV ] && echo 'Conda ENV not active!' && exit 1
 
 pushd $(pwd)/lib/federation-service/federation_service
+    # Using another enviroment in order to avoid packages conflict 
+    python3 -m venv federation_service_venv
+    source federation_service_venv/bin/activate
     pip install -r requirements.txt
     python -m candig_federation --host ${FEDERATION_SERVICE_IP} --port ${FEDERATION_SERVICE_PORT}
 popd
