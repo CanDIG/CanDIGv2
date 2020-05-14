239c464afe972d69634cf39af14d2299b3daff52
Felipe <fcoralsasso@bcgsc.ca>
2020-05-14 14:50:23 -0700

    Added run file for metadata_service

---
+++ lib/metadata_service/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+source .env
+
+[ -z $CONDA_DEFAULT_ENV ] && echo 'Conda ENV not active!' && exit 1
+
+pushd $(pwd)/lib/metadata_service/metadata_service_v2
+	pip install -r requirements.txt
+    ./manage.py runserver
+popd
