03f296b806261ecf0f77bcca06c880751158b0ef
Felipe <fcoralsasso@bcgsc.ca>
2020-05-26 11:33:06 -0700

    Commented mock data ingestion code

--- lib/candig_server/run.sh
+++ lib/candig_server/run.sh
@@ -9,11 +9,12 @@ source .env
 pushd $(pwd)/lib/candig_server/
     pip install candig-server
     pip install candig-ingest==1.3.1
-    mkdir candig-example-data
-    wget https://raw.githubusercontent.com/CanDIG/candig-ingest/master/candig/ingest/mock_data/clinical_metadata_tier1.json
-    wget https://raw.githubusercontent.com/CanDIG/candig-ingest/master/candig/ingest/mock_data/clinical_metadata_tier2.json
-    ingest candig-example-data/registry.db mock1 clinical_metadata_tier1.json
-    ingest candig-example-data/registry.db mock2 clinical_metadata_tier2.json
+    # Uncomment below lines if you want to ingest some mock data
+    # mkdir candig-example-data
+    # wget https://raw.githubusercontent.com/CanDIG/candig-ingest/master/candig/ingest/mock_data/clinical_metadata_tier1.json
+    # wget https://raw.githubusercontent.com/CanDIG/candig-ingest/master/candig/ingest/mock_data/clinical_metadata_tier2.json
+    # ingest candig-example-data/registry.db mock1 clinical_metadata_tier1.json
+    # ingest candig-example-data/registry.db mock2 clinical_metadata_tier2.json
 
     candig_server --host ${CANDIG_SERVER_HOST} --port ${CANDIG_SERVER_PORT}
 popd
