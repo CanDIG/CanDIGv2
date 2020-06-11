7812369baadd62a9fc06c599c5e1a940d95d379c
Shaikh Farhan Rashid <sk.f.rashid@gmail.com>
2020-06-11 19:10:14 -0400

    refactor of variables and docker-compose for some services

--- lib/federation-service/run.sh
+++ lib/federation-service/run.sh
@@ -7,7 +7,7 @@ source .env
 [ -z $CONDA_DEFAULT_ENV ] && echo 'Conda ENV not active!' && exit 1
 
 pushd $(pwd)/lib/federation-service/federation_service
-    # Using another enviroment in order to avoid packages conflict 
+    # Using another enviroment in order to avoid packages conflict
     python3 -m venv federation_service_venv
     source federation_service_venv/bin/activate
     pip install -r requirements.txt
