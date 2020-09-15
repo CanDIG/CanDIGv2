f6e411440af78e20ac85243ae9a99bfe5fad0aa5
Brennan Brouillette <brennan.brouillette.culliford@gmail.com>
2020-09-15 10:55:20 -0400

    bash is fked

--- etc/setup/scripts/setup.sh
+++ etc/setup/scripts/setup.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /usr/bin/env bash
 # -- Prerequisites --
 echo
 echo "- Generating prerequisites; -"
--- etc/setup/scripts/subtasks/vault_setup.sh
+++ etc/setup/scripts/subtasks/vault_setup.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /usr/bin/env bash
 # This script will set up a full vault environment on your local CanDIGv2 cluster
 
 
@@ -11,11 +11,11 @@ envsubst < ${PWD}/etc/setup/templates/configs/vault/vault-config.json.tpl > ${PW
 docker-compose -f ${PWD}/lib/vault/docker-compose.yml up -d
 
 # ssh into container
-# example keys/tokens (TODO: automate)
-KEY_1=5f1xhr4FdqobnMUmkoV2ewRhRGFXBQf+7DSjW8cgmvn+
-KEY_2=oMcAdjCm2UITy0fcAqvNEpk7jnfc278XffXq7HQgSDll
-KEY_3=TukMeCpJroaPF/4lEPOH2M8Cd/iX18nfsb7Pz8PAWX95
-ROOT_LOGIN_TOKEN=s.Zlbes4X0ER3Eaym5MKmpHBCa
+# # example keys/tokens (TODO: automate)
+# KEY_1=5f1xhr4FdqobnMUmkoV2ewRhRGFXBQf+7DSjW8cgmvn+
+# KEY_2=oMcAdjCm2UITy0fcAqvNEpk7jnfc278XffXq7HQgSDll
+# KEY_3=TukMeCpJroaPF/4lEPOH2M8Cd/iX18nfsb7Pz8PAWX95
+# ROOT_LOGIN_TOKEN=s.Zlbes4X0ER3Eaym5MKmpHBCa
 # --
 
 # -- todo: run only if not already initialized --
@@ -54,13 +54,14 @@ echo "${key_array[2]}"
 sleep 1
 
 # doesn't work yet ---
-echo ">> unsealing vault with keys ${key_array[0]}, ${key_array[1]}, ${key_array[2]}"
+#echo "unsealing vault with keys "#'${key_array[0]}', '${key_array[1]}', '${key_array[2]}'"
+echo "something"
 sleep 1
-export cmd1="vault operator unseal ${key_array[0]}" 
+export cmd1="vault operator unseal '${key_array[0]}'" 
 sleep 1
-export cmd2="vault operator unseal ${key_array[1]}"
+export cmd2="vault operator unseal '${key_array[1]}'"
 sleep 1
-export cmd3="vault operator unseal ${key_array[2]}"
+export cmd3="vault operator unseal '${key_array[2]}'"
 sleep 1
 
 echo "set command to ${cmd1}"
@@ -70,10 +71,10 @@ sleep 1
 echo "set command to ${cmd3}"
 sleep 1
 
-result=$(docker exec -it -e RUN_ME="${cmd1}" vault sh -c "$(echo "run me is $RUN_ME")")
+result=$(docker exec -it -e RUN_ME="${cmd1}" vault sh -c "$(echo '$RUN_ME')")
 echo $result
 sleep 1
-docker exec -it -e RUN_ME="${cmd2}" vault sh -c "$RUN_ME"
+docker exec -it vault sh -c "${cmd2}"
 sleep 1
 docker exec -it -e RUN_ME="${cmd3}" vault sh -c "$RUN_ME"
 sleep 1
@@ -83,7 +84,7 @@ sleep 1
 #docker exec -it vault sh -c "vault login $ROOT_LOGIN_TOKEN"
 
 # configuration
-docker exec -it vault sh -c "vault audit enable file file_path=/tmp/vault-audit.log"
+#docker exec -it vault sh -c "vault audit enable file file_path=/tmp/vault-audit.log"
 # ..
 # ---
 
