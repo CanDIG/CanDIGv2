34a4de70c28ac794f983548776498d303c5edd9d
shaikh-rashid <44211165+shaikh-rashid@users.noreply.github.com>
2021-03-25 13:04:57 -0400

    Update activate.sh

--- etc/venv/activate.sh
+++ etc/venv/activate.sh
@@ -1,5 +1,6 @@
 source $PWD/bin/miniconda3/etc/profile.d/conda.sh
 conda activate candig
 pip install -U -r $PWD/etc/venv/requirements.txt
+#pip install -U -r $PWD/etc/venv/requirements-dex.txt
 export PATH="$PWD/bin:$PATH"
-eval $(docker-machine env manager)
+#eval $(docker-machine env manager)
