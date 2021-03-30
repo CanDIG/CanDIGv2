8b2dde8d9fe74f71209bdad56039d130fc4707c2
daisie_local <daisieh@gmail.com>
2021-03-30 00:52:50 -0700

    only pip install once, in provisioning

--- provision.sh
+++ provision.sh
@@ -63,3 +63,4 @@ make init-conda
 source $PWD/bin/miniconda3/etc/profile.d/conda.sh
 conda activate candig
 pip install -U -r $PWD/etc/venv/requirements.txt
+#pip install -U -r $PWD/etc/venv/requirements-dev.txt
