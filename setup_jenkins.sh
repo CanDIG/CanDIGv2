178c3b3a2b4b1ec25dca21c09af208c190f7bcc9
daisie_local <daisieh@gmail.com>
2021-06-29 15:53:32 -0700

    update setup_jenkins

--- setup_jenkins.sh
+++ setup_jenkins.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
-
 cp etc/env/example.env .env
 
-tail -n 2 $WORKING_DIR/progress.txt | grep -q "finished init-conda"
-if [ $? -eq 0 ]; then
-  echo "hi"
-else
+grep -q "finished init-conda" $WORKING_DIR/progress.txt
+if [ $? -ne 0 ]; then
   echo "need to re-run setup"
   rm $WORKING_DIR/progress.txt
+  make bin-all
+  make init-conda
+  . $PWD/bin/miniconda3/etc/profile.d/conda.sh
+  conda activate candig
+  pip install -U -r $PWD/etc/venv/requirements.txt
+else
+  echo "already set up"
 fi
-
-make bin-all
-make init-conda
-. $PWD/bin/miniconda3/etc/profile.d/conda.sh
-conda activate candig
-pip install -U -r $PWD/etc/venv/requirements.txt
-
-cat $WORKING_DIR/progress.txt
