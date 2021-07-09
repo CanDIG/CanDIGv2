432b322193a9fbf594e006431342365c6ce540c9
daisie_local <daisieh@gmail.com>
2021-07-09 16:57:16 -0700

    touch logfile

--- setup_jenkins.sh
+++ setup_jenkins.sh
@@ -4,12 +4,11 @@ LOGFILE=$PWD/tmp/logs/progress.txt
 
 sed 's/DOCKER_REGISTRY=.*/DOCKER_REGISTRY=ghcr.io\/candig/' etc/env/example.env > .env
 
-# grep -q "finished init-conda" $LOGFILE
 . $PWD/bin/miniconda3/etc/profile.d/conda.sh
 conda activate candig
 if [ $? -ne 0 ]; then
   echo "need to re-run setup"
-  rm $LOGFILE
+  touch $LOGFILE
   make bin-all
   make init-conda
   . $PWD/bin/miniconda3/etc/profile.d/conda.sh
