f4cb67718695c377bf7a70cdc5e09c74fca6ae89
Daisie Huang <daisieh@gmail.com>
2021-07-12 11:08:44 -0700

    Initial jenkins setup (#82)
    * set a default value for WORKING_DIR
    
    * activate conda on login
    
    * working dir
    
    * oops
    
    * Update example.env
    
    * initial commit for setup_jenkins
    
    * test
    
    * disable toil-docker for now
    
    * update setup_jenkins
    
    * try editing env vars
    
    * try editing env vars
    
    * try editing env vars
    
    * don't push toil modules either
    
    * working_dir is just the wd
    
    * change location for progress.txt
    
    * try using conda activate as the test
    
    * touch logfile
    
    * touch logfile
    
    * touch logfile
    
    * move log file

--- provision.sh
+++ provision.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
 # Optional args: CanDIGv2 repo path, CanDIGv2 repo branch
-echo "started provision.sh" | tee -a ~/progress.txt
+
+LOGFILE=$PWD/tmp/progress.txt
+
+echo "started provision.sh" | tee -a $LOGFILE
 # sudo apt-get update
 sudo apt-get upgrade -y
-echo "  finished apt-get upgrade" | tee -a ~/progress.txt
+echo "  finished apt-get upgrade" | tee -a $LOGFILE
 
 # remove grub file and recreate, in case of version conflicts
 sudo rm /boot/grub/menu.lst
 sudo update-grub-legacy-ec2 -y
 sudo apt-get dist-upgrade -y
 sudo apt-get update
-echo "  finished apt-get dist-upgrade" | tee -a ~/progress.txt
+echo "  finished apt-get dist-upgrade" | tee -a $LOGFILE
 
-echo "  installing necessary packages" | tee -a ~/progress.txt
+echo "  installing necessary packages" | tee -a $LOGFILE
 declare -a install_pkgs=("build-essential" "zlib1g-dev" "libncurses5-dev" "libgdbm-dev" "libnss3-dev" "libssl-dev" "libreadline-dev" "libffi-dev" "wget" "curl" "git" "make" "gcc" "libsqlite3-dev" "libbz2-dev" "liblzma-dev" "lzma" "sqlite3" "apt-transport-https" "ca-certificates" "gnupg2" "software-properties-common")
 
 for pkg in ${install_pkgs[@]}; do
   sudo apt-get install -y $pkg
   if [ $? -ne 0 ]; then
-    echo "ERROR! Failed to install $pkg" | tee -a ~/progress.txt
+    echo "ERROR! Failed to install $pkg" | tee -a $LOGFILE
     exit 1
   fi
 done
 
-echo "  finished installs" | tee -a ~/progress.txt
+echo "  finished installs" | tee -a $LOGFILE
 
 if [ -n "$1" ]
 then
@@ -53,7 +56,7 @@ fi
 sudo chown -R $(whoami):$(whoami) $path
 git submodule update --init --recursive
 cp -i etc/env/example.env .env
-echo "  finished setting up CanDIGv2 repo" | tee -a ~/progress.txt
+echo "  finished setting up CanDIGv2 repo" | tee -a $LOGFILE
 
 dist=$(lsb_release -is)
 codename=$(lsb_release -cs)
@@ -63,7 +66,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/${di
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-echo "  finished installing docker components" | tee -a ~/progress.txt
+echo "  finished installing docker components" | tee -a $LOGFILE
 
 sudo apt-get autoclean
 sudo apt-get autoremove -y
@@ -73,19 +76,19 @@ sudo usermod -aG docker $(whoami)
 
 make bin-all
 if [ $? -ne 0 ]; then
-  echo "ERROR! Failed make bin-all" | tee -a ~/progress.txt
+  echo "ERROR! Failed make bin-all" | tee -a $LOGFILE
   exit 1
 fi
 
 make init-conda
 if [ $? -ne 0 ]; then
-  echo "ERROR! Failed make init-conda" | tee -a ~/progress.txt
+  echo "ERROR! Failed make init-conda" | tee -a $LOGFILE
   exit 1
 fi
 
-echo "  starting candig pip install" | tee -a ~/progress.txt
+echo "  starting candig pip install" | tee -a $LOGFILE
 source $PWD/bin/miniconda3/etc/profile.d/conda.sh
 conda activate candig
 pip install -U -r $PWD/etc/venv/requirements.txt
 #pip install -U -r $PWD/etc/venv/requirements-dev.txt
-echo "finished provision.sh" | tee -a ~/progress.txt
+echo "finished provision.sh" | tee -a $LOGFILE
