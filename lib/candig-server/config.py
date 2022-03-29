a30006251a5d9085adf176ede931eb980f3f5285
Daisie Huang <daisieh@gmail.com>
2022-03-29 12:23:49 -0700

    use config file

---
+++ lib/candig-server/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+OPA_SERVER = os.environ['OPA_SERVER']
+OPA_SERVER_TOKEN = os.environ['OPA_SERVER_TOKEN']
+
+TYK_ENABLED = True
+TYK_SERVER = os.environ['TYK_SERVER']
+TYK_LISTEN_PATH = os.environ['TYK_LISTEN_PATH']
