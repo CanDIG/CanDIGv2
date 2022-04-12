4000472833c7e35a5ec043a9a8704c88ac84ea88
Daisie Huang <daisieh@gmail.com>
2022-04-12 10:07:42 -0700

    tiny setting tweaks (#130)
    * update urls for portal
    
    * don't bother with test datasets in opa

--- lib/opa/opa_setup.sh
+++ lib/opa/opa_setup.sh
@@ -10,4 +10,4 @@ opa_runner=$(docker ps | grep "opa-runner" | awk '{print $1}')
 docker exec $opa_runner python3 app/permissions_engine/fetch_keys.py
 docker restart candigv2_opa_1
 
-docker exec $opa_runner python3 app/tests/create_katsu_test_datasets.py
+# docker exec $opa_runner python3 app/tests/create_katsu_test_datasets.py
