7a8a0b6ecc3027a9f7e47c3da0146d35c1585312
Sergiu Dumitriu <sergiu@phenotips.org>
2022-04-14 15:42:48 -0400

    Fix opa container name in the opa_setup script

--- lib/opa/opa_setup.sh
+++ lib/opa/opa_setup.sh
@@ -8,6 +8,6 @@ LOGFILE=$PWD/tmp/progress.txt
 opa_runner=$(docker ps | grep "opa-runner" | awk '{print $1}')
 
 docker exec $opa_runner python3 app/permissions_engine/fetch_keys.py
-docker restart candigv2_opa_1
+docker restart candig_opa_1
 
 # docker exec $opa_runner python3 app/tests/create_katsu_test_datasets.py
