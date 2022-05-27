ce11e6af51f4b2d57c5b577ecd2bda9af2794fad
Daisie Huang <daisieh@gmail.com>
2022-05-26 23:55:12 -0700

    reorg vault so that we can do more active token refreshing

---
+++ lib/vault/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -Euo pipefail
+
+
+if [[ -f "initial_setup" ]]; then
+    rm initial_setup
+else
+    # unseal vault
+    KEY=$(head -n 2 /vault/config/keys.txt | tail -n 1)
+    echo '{ "key": "'$KEY'" }' > payload.json
+    curl --request POST --data @payload.json http://vault:8200/v1/sys/unseal
+    KEY=$(head -n 3 /vault/config/keys.txt | tail -n 1)
+    echo '{ "key": "'$KEY'" }' > payload.json
+    curl --request POST --data @payload.json http://vault:8200/v1/sys/unseal
+    KEY=$(head -n 4 /vault/config/keys.txt | tail -n 1)
+    echo '{ "key": "'$KEY'" }' > payload.json
+    curl --request POST --data @payload.json http://vault:8200/v1/sys/unseal
+    
+fi
+
+ROOT=$(tail -n 1 /vault/config/keys.txt)
+export VAULT_S3_TOKEN=$(cat /run/secrets/vault-s3-token)
+echo '{ "id": "'$VAULT_S3_TOKEN'", "policies": ["aws"], "period":"5h" }' > token.json
+curl --request POST --header "X-Vault-Token: "$ROOT"" --data @token.json http://vault:8200/v1/auth/token/create
+
+top
