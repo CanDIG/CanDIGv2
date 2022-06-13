e441f9e4c3da0d8b7abbf3f85cdc3f3e5cd858e5
Daisie Huang <daisieh@gmail.com>
2022-06-13 15:45:18 -0700

    move creation of token to separate script

--- lib/vault/entrypoint.sh
+++ lib/vault/entrypoint.sh
@@ -20,9 +20,6 @@ else
     
 fi
 
-ROOT=$(tail -n 1 /vault/config/keys.txt)
-export VAULT_S3_TOKEN=$(cat /run/secrets/vault-s3-token)
-echo '{ "id": "'$VAULT_S3_TOKEN'", "policies": ["aws"], "period":"5h" }' > token.json
-curl --request POST --header "X-Vault-Token: "$ROOT"" --data @token.json http://vault:8200/v1/auth/token/create
+bash create_token.sh
 
 top
