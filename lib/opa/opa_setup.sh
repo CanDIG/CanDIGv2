aee6d61c404368ddd4837a4a5c0838f78a38ccf0
Brennan Brouillette <brennan.brouillette@computationalgenomics.ca>
2022-09-16 17:17:14 -0400

    env, templates and scripts update

--- lib/opa/opa_setup.sh
+++ lib/opa/opa_setup.sh
@@ -6,7 +6,7 @@ LOGFILE=$PWD/tmp/progress.txt
 
 
 opa_runner=$(docker ps --format "{{.Names}}" | grep "opa-runner" | awk '{print $1}')
-opa_container=$(docker ps --format "{{.Names}}" | grep "opa_" | awk '{print $1}')
+opa_container=$(docker ps --format "{{.Names}}" | grep "opa-" | awk '{print $1}')
 
 docker exec $opa_runner python3 app/permissions_engine/fetch_keys.py
 docker restart $opa_container
