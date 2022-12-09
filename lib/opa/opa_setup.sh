fbcf32e662357d58e5021efb4e50f5c99032fdd4
Daisie Huang <daisieh@gmail.com>
2022-12-08 17:14:41 -0800

    start opa container even if exited

--- lib/opa/opa_setup.sh
+++ lib/opa/opa_setup.sh
@@ -5,10 +5,10 @@ set -Euo pipefail
 LOGFILE=$PWD/tmp/progress.txt
 
 
-opa_runner=$(docker ps --format "{{.Names}}" | grep "opa-runner" | awk '{print $1}')
-opa_container=$(docker ps --format "{{.Names}}" | grep "opa_" | awk '{print $1}')
+opa_runner=$(docker ps -a --format "{{.Names}}" | grep "opa-runner" | awk '{print $1}')
+opa_container=$(docker ps -a --format "{{.Names}}" | grep "opa_" | awk '{print $1}')
 
 docker exec $opa_runner python3 app/permissions_engine/fetch_keys.py
-docker restart $opa_container
+docker start $opa_container
 
 # docker exec $opa_runner python3 app/tests/create_katsu_test_datasets.py
