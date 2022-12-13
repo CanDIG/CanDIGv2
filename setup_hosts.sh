08880c5a643ca25a71e98f59d89612dc5f13259e
fnguyen <OrdinaryNeumann@gmail.com>
2022-12-13 11:04:19 -0500

    [Misc] Fix bug where echo was removing newlines

--- setup_hosts.sh
+++ setup_hosts.sh
@@ -11,25 +11,25 @@ fi
 # Replace docker.localhost entry in /etc/hosts
 echo "HACK ALERT"
 echo "This step will introduce an entry into /etc/hosts so that requests to docker.localhost"
-echo "will bounce off your local step. Root access will be required..."
+echo "will bounce off your local network. Root access will be required..."
 cp /etc/hosts .hosts.tmp
 printf "\n" >>.hosts.tmp
 
 if [ "$VENV_OS" == "linux" ]; then
-  IP=$(ifconfig | grep -A 1 'wlp0\|eth0' | grep -o "inet [0-9.]\+" | cut -d' ' -f2)
+  ifconfig | grep -A 1 'wlp0\|eth0' | grep -o "inet [0-9.]\+" | cut -d' ' -f2 > .hosts.tmp2
 else
-  IP=$(ifconfig | grep -o "inet [0-9.]\+" | cut -d' ' -f2)
+  ifconfig | grep -o "inet [0-9.]\+" | cut -d' ' -f2 > .hosts.tmp2
 fi
 
-numlines=$(echo "$IP" | wc -l)
+numlines=$(wc -l .hosts.tmp2)
 if [ "$numlines" == "1" ]; then
     printf "$IP\tdocker.localhost" >> .hosts.tmp
     sudo mv .hosts.tmp /etc/hosts;
+    rm .hosts.tmp2;
 else
     echo "ERROR: More than one IP has been detected. Since this script can't automatically determine which one"
     echo "will make the build work, please remove all but one of the inserted docker.localhost lines in /etc/hosts."
-    echo $IP >.hosts.tmp2
     awk '{printf "%s\tdocker.localhost\n", $0}' .hosts.tmp2 >> .hosts.tmp
-    rm .hosts.tmp2
     sudo mv .hosts.tmp /etc/hosts;
+    rm .hosts.tmp2;
 fi
