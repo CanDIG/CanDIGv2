3914f8d9ca91b84368dbbc6465a8d4c13f0d6fbd
Daisie Huang <daisieh@gmail.com>
2022-12-15 20:10:49 -0800

    add minio vars

--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ def main():
     vars["VAULT_URL"] = vars["CANDIG_URL"] + "/vault"
     vars["OPA_URL"] = vars["CANDIG_URL"] + "/policy"
     vars["OPA_SITE_ADMIN_KEY"] = candigv2_env["OPA_SITE_ADMIN_KEY"]
+    vars["MINIO_URL"] = candigv2_env["MINIO_PUBLIC_URL"]
 
     # vars that come from files:
     with open(f"tmp/secrets/opa-root-token") as f:
@@ -28,6 +29,10 @@ def main():
         vars["VAULT_S3_TOKEN"] = f.read().splitlines().pop()
     with open(f"tmp/vault/keys.txt") as f:
         vars["VAULT_ROOT_TOKEN"] = f.read().splitlines().pop(-1)
+    with open(f"tmp/secrets/minio-access-key") as f:
+        vars["MINIO_ACCESS_KEY"] = f.read().splitlines().pop()
+    with open(f"tmp/secrets/minio-secret-key") as f:
+        vars["MINIO_SECRET_KEY"] = f.read().splitlines().pop()
 
     with open("env.sh", "w") as f:
         for key in vars.keys():
