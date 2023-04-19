dd5034d9b1fc8c5742d90eda375a561b63dcebd4
Daisie Huang <daisieh@gmail.com>
2023-04-18 21:32:35 -0700

    add templates of preflight/setup scripts

---
+++ lib/templates/templates_preflight.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+LOGFILE=$PWD/tmp/progress.txt
+
+# This script runs before the container is composed.
---
+++ lib/templates/templates_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+LOGFILE=$PWD/tmp/progress.txt
+
+# This script runs after the container is composed.
