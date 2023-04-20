b149ec281e3736f63d61e6e10e04ffdecee61a17
fnguyen <OrdinaryNeumann@gmail.com>
2023-04-20 13:57:15 -0400

    Fix an issue with pre-build-check which caused it to fail on certain MacOS systems

--- pre-build-check.sh
+++ pre-build-check.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-CANDIG_HOST=`getent hosts candig-dev`
+if command -v getent >/dev/null 2>&1; then
+    CANDIG_HOST=`getent hosts candig-dev`
+elif command -v dnscacheutil >/dev/null 2>&1; then
+    CANDIG_HOST=`dscacheutil -q host -a name candig-dev`
+fi
+
 if [ ! -z "$CANDIG_HOST" ]; then
     printf "Please disable the UHN VPN, as it causes errors with the build process"
     exit 1
