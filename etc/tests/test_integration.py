6c04db28f956be508f8b6440e75475490b00b115
Daisie Huang <daisieh@gmail.com>
2023-04-27 18:59:45 -0700

    Update test_integration.py

--- etc/tests/test_integration.py
+++ etc/tests/test_integration.py
@@ -154,7 +154,7 @@ def test_vault():
 def test_htsget():
     old_val = os.environ.get("TESTENV_URL")
     os.environ['TESTENV_URL'] = f"http://{ENV['CANDIG_ENV']['CANDIG_DOMAIN']}:{ENV['CANDIG_ENV']['HTSGET_APP_PORT']}"
-    retcode = pytest.main(["-x", "lib/htsget-server/htsget_app/tests/test_htsget_server.py"])
+    retcode = pytest.main(["-x", "lib/htsget/htsget_app/tests/test_htsget_server.py"])
     if old_val is not None:
         os.environ['TESTENV_URL'] = old_val
     print(retcode)
