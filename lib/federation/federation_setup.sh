c2cb3f15f04165e38a0fb9d14f67abc5fcbc8b8a
Daisie Huang <daisieh@gmail.com>
2023-05-05 10:51:28 -0700

    Update federation_setup.sh

--- lib/federation/federation_setup.sh
+++ lib/federation/federation_setup.sh
@@ -6,14 +6,14 @@ LOGFILE=$PWD/tmp/progress.txt
 
 # This script runs after the container is composed.
 
-echo ">> waiting for federation-service to start"
-docker ps --format "{{.Names}}" | grep federation-service_1
+echo ">> waiting for federation to start"
+docker ps --format "{{.Names}}" | grep federation_1
 while [ $? -ne 0 ]
 do
   echo "..."
   sleep 1
-  docker ps --format "{{.Names}}" | grep federation-service_1
+  docker ps --format "{{.Names}}" | grep federation_1
 done
 
 source env.sh
-python $PWD/lib/federation-service/initialize.py
+python $PWD/lib/federation/initialize.py
