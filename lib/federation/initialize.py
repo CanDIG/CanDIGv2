b83851e8b3d95501b8ee74873179acf716db091f
Daisie Huang <daisieh@gmail.com>
2023-08-14 12:06:54 -0700

    fix formatting strings

--- lib/federation/initialize.py
+++ lib/federation/initialize.py
@@ -41,7 +41,7 @@ def main():
     headers["Authorization"] = f"Bearer {token}"
 
     print("Adding servers to federation...")
-    url = f"http://localhost:{get_env_value("FEDERATION_PORT")}/federation/v1/servers"
+    url = f"http://localhost:{get_env_value('FEDERATION_PORT')}/federation/v1/servers"
     response = requests.request("POST", url, headers=headers, json=server)
     # add other federated servers here
     if response.status_code != 200:
@@ -52,11 +52,11 @@ def main():
 
     print("Adding services to federation...")
     services = find_services()
-    url = f"http://localhost:{get_env_value("FEDERATION_PORT")}/federation/v1/services"
+    url = f"http://localhost:{get_env_value('FEDERATION_PORT')}/federation/v1/services"
     for service in services:
         response = requests.request("POST", url, headers=headers, json=service)
 
-    url = f"http://localhost:{get_env_value("FEDERATION_PORT")}/federation/v1/services"
+    url = f"http://localhost:{get_env_value('FEDERATION_PORT')}/federation/v1/services"
     response = requests.request("GET", url, headers=headers)
     print(response.text)
 
