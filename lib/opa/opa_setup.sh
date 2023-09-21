d87899e157dee2b833ee52791ad63f304234e0a4
Daisie Huang <daisieh@gmail.com>
2023-09-21 16:14:39 -0700

    this will have already run on entrypoint.sh

--- lib/opa/opa_setup.sh
+++ lib/opa/opa_setup.sh
@@ -19,7 +19,6 @@ sleep 5
 opa_runner=$(docker ps -a --format "{{.Names}}" | grep "opa-runner" | awk '{print $1}')
 opa_container=$(docker ps -a --format "{{.Names}}" | grep "opa_" | awk '{print $1}')
 
-docker exec $opa_runner python3 app/permissions_engine/fetch_keys.py
 docker start $opa_container
 
 # docker exec $opa_runner python3 app/tests/create_katsu_test_datasets.py
