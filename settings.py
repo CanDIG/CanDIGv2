086dd6753fd66d43cb3f9fcc01e5372b421c8132
OrdiNeu <OrdinaryNeumann@gmail.com>
2023-10-12 16:59:18 -0400

    Add requirements for HTSGet to access Postgres (#296)
    Add requirements for HTSGet to access Postgres

--- settings.py
+++ settings.py
@@ -72,7 +72,9 @@ def get_env():
         vars["MINIO_SECRET_KEY"] = f.read().splitlines().pop()
     with open(f"tmp/secrets/tyk-secret-key") as f:
         vars["TYK_SECRET_KEY"] = f.read().splitlines().pop()
+    vars["POSTGRES_PASSWORD_FILE"] = f"tmp/secrets/metadata-db-secret"
     vars["CANDIG_ENV"] = INTERPOLATED_ENV
+    vars["DB_PATH"] = "metadata-db"
     return vars
 
 
