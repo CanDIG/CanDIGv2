9fd51989865608258709666d5dc42f9d4c64343c
Daisie Huang <daisieh@gmail.com>
2023-10-27 15:20:09 -0700

    fix create token

--- lib/vault/create_token.sh
+++ lib/vault/create_token.sh
@@ -6,4 +6,5 @@ set -Euo pipefail
 export ROOT=$(tail -n 1 /vault/config/keys.txt)
 export VAULT_S3_TOKEN=$(cat /run/secrets/vault-s3-token)
 echo '{ "id": "'$VAULT_S3_TOKEN'", "policies": ["aws"], "period":"5h" }' > token.json
+echo "renewing s3 token"
 curl --request POST --header "X-Vault-Token: "$ROOT"" --data @token.json $VAULT_URL/v1/auth/token/create
