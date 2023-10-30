a67527d9e32f662159d6226d1b2e5424a841e7e1
Daisie Huang <daisieh@gmail.com>
2023-10-30 10:11:26 -0700

    DIG-1301: delete cascades, rename htsget dataset to cohort (#312)
    * rename dataset to cohort
    
    * fix create token
    
    * Update test_integration.py
    
    * Update htsget_app
    
    * Update htsget_app

--- lib/vault/entrypoint.sh
+++ lib/vault/entrypoint.sh
@@ -27,7 +27,7 @@ fi
 bash /vault/create_token.sh
 # set up crontab
 crontab -l > cron_bkp
-echo "0 */5 * * * bash /vault/create_token.sh" >> cron_bkp
+echo -e "0\t*/5\t*\t*\t*\tbash /vault/create_token.sh" >> cron_bkp
 crontab cron_bkp
 rm cron_bkp
 crond
