ccc0b4a7228626e06355df5380c79152f5094565
Daisie Huang <daisieh@gmail.com>
2023-12-18 14:00:01 -0800

    remove references for requirements-dev.txt (#358)

--- etc/venv/activate.sh
+++ etc/venv/activate.sh
@@ -1,6 +1,5 @@
 source $PWD/bin/miniconda3/etc/profile.d/conda.sh
 conda activate candig
 pip install -U -r $PWD/etc/venv/requirements.txt
-#pip install -U -r $PWD/etc/venv/requirements-dev.txt
 export PATH="$PWD/bin:$PATH"
 #eval $(docker-machine env manager)
