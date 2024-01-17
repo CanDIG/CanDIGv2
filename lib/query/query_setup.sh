20d938f81f0bbbc7417f1cb643a646dcceaf84f2
Daisie Huang <daisieh@gmail.com>
2024-01-17 10:38:15 -0800

    restart container after create_service_store

--- lib/query/query_setup.sh
+++ lib/query/query_setup.sh
@@ -11,15 +11,16 @@ python settings.py
 source env.sh
 
 echo ">> waiting for query to start"
-docker ps --format "{{.Names}}" | grep query_1
+query=$(docker ps --format "{{.Names}}" | grep query_1)
 while [ $? -ne 0 ]
 do
   echo "..."
   sleep 1
-  docker ps --format "{{.Names}}" | grep query_1
+  query=$(docker ps --format "{{.Names}}" | grep query_1)
 done
 sleep 5
 
 
 bash $PWD/create_service_store.sh "query"
 
+docker restart $query
--- lib/templates/templates_setup.sh
+++ lib/templates/templates_setup.sh
@@ -11,15 +11,16 @@ python settings.py
 source env.sh
 
 echo ">> waiting for templates to start"
-docker ps --format "{{.Names}}" | grep templates_1
+template_container=$(docker ps --format "{{.Names}}" | grep templates_1)
 while [ $? -ne 0 ]
 do
   echo "..."
   sleep 1
-  docker ps --format "{{.Names}}" | grep templates_1
+  template_container=$(docker ps --format "{{.Names}}" | grep templates_1)
 done
 sleep 5
 
 
 bash $PWD/create_service_store.sh "templates"
 
+docker restart $template_container
