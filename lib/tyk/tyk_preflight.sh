a20b04ee57ef546efda17a4c7cfc21e6c92c1688
Daisie Huang <daisieh@gmail.com>
2024-01-18 12:46:29 -0800

    DIG-1438: updates to make system, tweaks to vault setup (#386)
    * updating submodules (#388)
    
    Co-authored-by: github-actions <github-actions@github.com>
    
    * add export SERVICE_NAME in more places
    
    * don't clean-bin every time we clean-all
    
    * tmp/vault has secrets to clean
    
    * improve counting of modules
    
    * add auth modules to CANDIG_MODULES
    
    * for each module, build/compose together
    
    * don't count auth modules twice
    
    * add some checks for software being installed
    
    * write into a service_stores.txt file
    
    * only actually create the store if vault is running
    
    * if vault already initialized, unseal and login
    
    * in case we're re-composing a live container, touch initial_setup again
    
    * change env var VAULT_SERVICE_URL to VAULT_PRIVATE_URL to match convention
    
    * Update example.env
    
    * if service stores have been created before, create again
    
    * add stop-all and start-all targets
    
    * semicolon
    
    * found a few bits where tyk didn't know about query
    
    ---------
    
    Co-authored-by: candigbot <87504143+candigbot@users.noreply.github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- lib/tyk/tyk_preflight.sh
+++ lib/tyk/tyk_preflight.sh
@@ -228,6 +228,9 @@ mv lib/tyk/tmp/tmp_policies.json.tpl lib/tyk/tmp/policies.json.tpl
 echo "Working on api_query.json" | tee -a $LOGFILE
 envsubst < ${PWD}/lib/tyk/configuration_templates/api_query.json.tpl > ${CONFIG_DIR}/apps/${TYK_QUERY_API_ID}.json
 
+echo "Working on api_query.json" | tee -a $LOGFILE
+envsubst < ${PWD}/lib/tyk/configuration_templates/api_query.json.tpl > ${CONFIG_DIR}/apps/${TYK_QUERY_API_ID}.json
+
 # Extra APIs can be added here
 # echo "Working on api_example.json"
 # envsubst < ${PWD}/lib/tyk/configuration_templates/api_example.json.tpl > ${CONFIG_DIR}/apps/${TYK_EXAMPLE_API_ID}.json
