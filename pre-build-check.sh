055ee0ec3c23f199f3969eee83b68be96d9e6cab
Daisie Huang <daisieh@gmail.com>
2024-01-19 10:23:16 -0800

    exit if not installed

--- pre-build-check.sh
+++ pre-build-check.sh
@@ -67,16 +67,19 @@ fi
 envsubst --version
 if [[ $? -ne 0 ]]; then
     echo "envsubst/gettext is not installed on your machine. Install gettext with your package manager of choice."
+    exit 1
 fi
 
 # Check 5: is jq installed?
 jq --version
 if [[ $? -ne 0 ]]; then
     echo "jq is not installed on your machine. Follow instructions at https://jqlang.github.io/jq/download/"
+    exit 1
 fi
 
 # Check 6: is yq installed?
 yq --version
 if [[ $? -ne 0 ]]; then
     echo "yq is not installed on your machine. Follow instructions at https://github.com/mikefarah/yq/#install"
+    exit 1
 fi
