7e2c56f31640c625470e66240be2eb3f53e5d99b
Daisie Huang <daisieh@gmail.com>
2024-01-18 16:27:21 -0800

    Update tyk_preflight.sh

--- lib/tyk/tyk_preflight.sh
+++ lib/tyk/tyk_preflight.sh
@@ -192,6 +192,18 @@ mv lib/tyk/tmp/tmp_policies.json.tpl lib/tyk/tmp/policies.json.tpl
 
 echo "Working on api_query.json" | tee -a $LOGFILE
 envsubst < ${PWD}/lib/tyk/configuration_templates/api_query.json.tpl > ${CONFIG_DIR}/apps/${TYK_QUERY_API_ID}.json
+cat lib/tyk/tmp/policies.json.tpl | jq '.["${TYK_POLICY_ID}"]["access_rights"] +=
+{"${TYK_QUERY_API_ID}": {
+    "allowed_urls": [],
+    "api_id": "${TYK_QUERY_API_ID}",
+    "api_name": "${TYK_QUERY_API_SLUG}",
+    "versions": [
+        "Default"
+    ]
+}
+}
+' > lib/tyk/tmp/tmp_policies.json.tpl
+mv lib/tyk/tmp/tmp_policies.json.tpl lib/tyk/tmp/policies.json.tpl
 
 # Extra APIs can be added here
 # echo "Working on api_example.json"
