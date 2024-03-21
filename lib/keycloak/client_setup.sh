bad29f82e14de6f97b12ba5af896c115d74e1185
Son Chau <soncqb@gmail.com>
2024-03-21 08:43:52 -0700

    Update client_setup.sh

--- lib/keycloak/client_setup.sh
+++ lib/keycloak/client_setup.sh
@@ -1,51 +1,33 @@
-# This script creates and configures a client within a Keycloak realm:
-# 1. Checks if a client exists. If not, it creates a new client.
-# 2. Updates the client configurations.
+# This script creates and configures a client within a Keycloak realm
 
 echo
 echo -e "${BLUE}Creating client: $KEYCLOAK_CLIENT_ID${DEFAULT}"
 
-getClient() {
-    local REALM_NAME=$1
-    local CLIENT_ID=$2
+CLIENT_OUTPUT=$($KCADM create clients -r "$KEYCLOAK_REALM" \
+    -s clientId="$KEYCLOAK_CLIENT_ID" \
+    -s enabled=true \
+    -s protocol=openid-connect \
+    -s publicClient=false \
+    -s clientAuthenticatorType=client-secret \
+    -s standardFlowEnabled=true \
+    -s directAccessGrantsEnabled=true \
+    -s 'redirectUris=["'"$TYK_LOGIN_TARGET_URL$KEYCLOAK_LOGIN_REDIRECT_PATH"'"]' \
+    -s 'webOrigins=["'"$TYK_LOGIN_TARGET_URL"'"]' 2>&1)
 
-    local ID=$($KCADM get clients -r "$REALM_NAME" --fields id,clientId | jq -r --arg CLIENT_ID "$CLIENT_ID" '.[] | select(.clientId==$CLIENT_ID) | .id')
-    echo "$ID"
-}
-
-createClient() {
-    local REALM_NAME=$1
-    local CLIENT_ID=$2
-    
-    local ID=$(getClient "$REALM_NAME" "$CLIENT_ID")
-    if [[ -z "$ID" ]]; then
-        $KCADM create clients -r "$REALM_NAME" -s clientId="$CLIENT_ID" -s enabled=true
-        ID=$(getClient "$REALM_NAME" "$CLIENT_ID") # Re-fetch ID of the newly created client
-    else
-        echo "Client $CLIENT_ID already exists." >&2
-    fi
-    echo "$ID"
-}
-
-ID=$(createClient "$KEYCLOAK_REALM" "$KEYCLOAK_CLIENT_ID")
-$KCADM update clients/"$ID" -r "$KEYCLOAK_REALM" \
-  -s protocol=openid-connect \
-  -s publicClient=false \
-  -s clientAuthenticatorType=client-secret \
-  -s standardFlowEnabled=true \
-  -s directAccessGrantsEnabled=true \
-  -s 'redirectUris=["'${TYK_LOGIN_TARGET_URL}${KEYCLOAK_LOGIN_REDIRECT_PATH}'"]' \
-  -s 'webOrigins=["'${TYK_LOGIN_TARGET_URL}'"]'
+echo $CLIENT_OUTPUT
+# Extract the client ID from the output
+CLIENT_ID=$(echo $CLIENT_OUTPUT | grep -oE '[0-9a-fA-F-]{36}')
 
 # Create client scopes
-$KCADM create clients/$ID/protocol-mappers/models -r $KEYCLOAK_REALM \
-	-s name=${KEYCLOAK_CLIENT_ID}-audience \
+SCOPE_NAME="${KEYCLOAK_CLIENT_ID}-audience"
+$KCADM create clients/$CLIENT_ID/protocol-mappers/models -r $KEYCLOAK_REALM \
+    -s name=$SCOPE_NAME \
     -s protocol=openid-connect \
-	-s protocolMapper=oidc-audience-mapper \
+    -s protocolMapper=oidc-audience-mapper \
     -s config="{\"included.client.audience\" : \"$KEYCLOAK_CLIENT_ID\",\"id.token.claim\" : \"true\",\"access.token.claim\" : \"true\"}"
 
 # EXPORT: Get the client secret and save it to secrets
-CLIENT_SECRET=$($KCADM get clients/"$ID"/client-secret -r "$KEYCLOAK_REALM" | jq -r '.value')
+CLIENT_SECRET=$($KCADM get clients/"$CLIENT_ID"/client-secret -r "$KEYCLOAK_REALM" | jq -r '.value')
 echo "$CLIENT_SECRET" > tmp/secrets/keycloak-client-$KEYCLOAK_CLIENT_ID-secret
 
 # EXPORT: Encode the Keycloak client ID in base64 and save it to secrets
