f1d603a44fea90524d7efcd43234fade1699f756
Son Chau <soncqb@gmail.com>
2024-03-27 16:01:40 -0700

    Sonchau/keycloak (#513)
    * remove old files
    
    * add new files
    
    * Update keycloak_setup.sh
    
    * Update docker-compose.yml
    
    * Update .gitignore
    
    * Update example.env
    
    * clean up old instructions
    
    * update Tyk logout to work with new keycloak

---
+++ lib/keycloak/user_setup.sh
@@ -0,0 +1,26 @@
+# This script creates and configures users within a Keycloak realm
+
+echo
+echo -e "${BLUE}Creating users${DEFAULT}"
+
+create_user_and_set_password() {
+    local USERNAME=$1
+    local PASSWORD=$2
+    local EMAIL=$3
+    local FIRST_NAME=$4
+    local LAST_NAME=$5
+
+    CREATE_OUTPUT=$(KCADM create users -r "$KEYCLOAK_REALM" \
+        -s username="$USERNAME" \
+        -s enabled=true \
+        -s email="$EMAIL" \
+        -s firstName="$FIRST_NAME" \
+        -s lastName="$LAST_NAME" 2>&1)
+    # uncomment the line beblow to see the output
+    # echo $CREATE_OUTPUT
+    KCADM set-password -r "$KEYCLOAK_REALM" --username "$USERNAME" --new-password "$PASSWORD"
+}
+
+# params: username password email firstname lastname
+create_user_and_set_password "$(cat tmp/secrets/keycloak-test-user)" "$(cat tmp/secrets/keycloak-test-user-password)" "user1@test.ca" "One" "User"
+create_user_and_set_password "$(cat tmp/secrets/keycloak-test-user2)" "$(cat tmp/secrets/keycloak-test-user2-password)" "user2@test.ca" "Two" "User"
