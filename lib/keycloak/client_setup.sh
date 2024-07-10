550d28ae0d21c1054c10e0c28d5c32a9dfba5ec8
Son Chau <soncqb@gmail.com>
2024-07-10 12:08:59 -0700

    remove debug comments

--- lib/keycloak/client_setup.sh
+++ lib/keycloak/client_setup.sh
@@ -1,9 +1,7 @@
 # This script creates and configures a client within a Keycloak realm
-
-echo
 echo -e "${BLUE}Creating client: $KEYCLOAK_CLIENT_ID${DEFAULT}"
 
-CREATE_OUTPUT=$(KCADM create clients -r "$KEYCLOAK_REALM" \
+CREATE_OUTPUT=$(KCADM -full create clients -r "$KEYCLOAK_REALM" \
     -s clientId="$KEYCLOAK_CLIENT_ID" \
     -s enabled=true \
     -s protocol=openid-connect \
@@ -13,25 +11,19 @@ CREATE_OUTPUT=$(KCADM create clients -r "$KEYCLOAK_REALM" \
     -s directAccessGrantsEnabled=true \
     -s 'redirectUris=["'"$TYK_LOGIN_TARGET_URL$KEYCLOAK_LOGIN_REDIRECT_PATH"'"]' \
     -s 'webOrigins=["'"$TYK_LOGIN_TARGET_URL"'"]' 2>&1)
-# uncomment the line beblow to see the output
-# echo $CREATE_OUTPUT
 
 # Extract the client ID from the output
 CLIENT_ID=$(echo $CREATE_OUTPUT | grep -oE '[0-9a-fA-F-]{36}')
 
 # Create client scopes
 SCOPE_NAME="${KEYCLOAK_CLIENT_ID}-audience"
-CREATE_OUTPUT=$(KCADM create clients/$CLIENT_ID/protocol-mappers/models -r $KEYCLOAK_REALM \
+KCADM create clients/$CLIENT_ID/protocol-mappers/models -r $KEYCLOAK_REALM \
     -s name=$SCOPE_NAME \
     -s protocol=openid-connect \
     -s protocolMapper=oidc-audience-mapper \
-    -s config="{\"included.client.audience\" : \"$KEYCLOAK_CLIENT_ID\",\"id.token.claim\" : \"true\",\"access.token.claim\" : \"true\"}" 2>&1)
-# uncomment the line below to see the output
-# echo $CREATE_OUTPUT
+    -s config="{\"included.client.audience\" : \"$KEYCLOAK_CLIENT_ID\",\"id.token.claim\" : \"true\",\"access.token.claim\" : \"true\"}"
 
-# EXPORT: Get the client secret and save it to secrets
-# echo $CLIENT_ID
-# echo $(KCADM get clients/"$CLIENT_ID"/client-secret -r "$KEYCLOAK_REALM")
-CLIENT_SECRET=$(docker exec "$KEYCLOAK_CONTAINER_ID" /opt/keycloak/bin/kcadm.sh get clients/"$CLIENT_ID"/client-secret -r "$KEYCLOAK_REALM" | sed -n '/{/,/}/p' | jq -r '.value')
-# echo $CLIENT_SECRET
+
+# EXPORT client secret
+CLIENT_SECRET=$(KCADM -full get clients/"$CLIENT_ID"/client-secret -r "$KEYCLOAK_REALM" | sed -n '/{/,/}/p' | jq -r '.value')
 echo "$CLIENT_SECRET" > tmp/keycloak/client-secret
