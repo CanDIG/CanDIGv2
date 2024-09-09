e7e333061ab3de45abe8b42844a4187234b1d1cd
fnguyen <OrdinaryNeumann@gmail.com>
2024-09-09 14:30:51 -0400

    Apply suggestions by @daisieh to refresh a refresh token instead of using username/password/url

--- etc/tests/test-endpoints-all.py
+++ etc/tests/test-endpoints-all.py
@@ -1,4 +1,5 @@
 import argparse
+import jwt
 import os
 import re
 import requests
@@ -25,42 +26,41 @@ parser.add_argument(
     required=True
 )
 parser.add_argument(
-    '--username',
-    help='username to login as'
-)
-parser.add_argument(
-    '--password',
-    help='password to login as'
-)
-parser.add_argument(
-    '--loginurl',
-    help='URL to hit when grabbing a token'
+    '--token',
+    help='refresh token for the target url',
+    required=True
 )
 parser.add_argument(
     '--secret',
-    help='URL to hit when grabbing a token'
+    help='client secret for the target',
+    required=True
 )
 
-def get_token(username, password, secret, login_url):
+
+def grab_new_refresh_token(refresh_token, secret):
+    decoded_token = jwt.decode(refresh_token, options={"verify_signature": False})
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
     data = [
-        ("grant_type", "password"),
-        ("client_id", "local_candig"),
+        ("grant_type", "refresh_token"),
+        ("client_id", decoded_token["azp"]),
         ("client_secret", secret),
-        ("username", username),
-        ("password", password),
+        ("refresh_token", refresh_token),
         ("scope", "openid")
     ]
-    retval = requests.post(login_url, headers=headers, data=data)
-    print(retval.text)
+
+    retval = requests.post(
+        decoded_token["iss"] + "/protocol/openid-connect/token",
+        headers=headers,
+        data=data
+        )
 
     token_re = re.search(r'refresh_token":"([a-zA-Z0-9._\-]+)"', retval.text)
     token = token_re.group(1)
     return token
 
-
 def stress_test():
     args = parser.parse_args()
+    refresh_token = args.token
     for line in args.input:
         # Allow other methods
         split = line.strip().split(" ")
@@ -70,15 +70,16 @@ def stress_test():
         extra = split[1:]
         sanitized_url = re.sub(r'https?://', "", url)
         sanitized_url = re.sub(r'/', "-", sanitized_url)
-        token = get_token(args.username, args.password, args.secret, args.loginurl)
         print(url)
+
+        refresh_token = grab_new_refresh_token(refresh_token, args.secret)
 
         rc = subprocess.run(
             ['python', os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test-endpoint.py'),
             '--url', url,
             '--out', os.path.join(args.out, sanitized_url + ".csv"),
             '--silent',
-            '-H', f"Authorization: Bearer {token}"] + extra)
+            '-H', f"Authorization: Bearer {refresh_token}"] + extra)
 
 if __name__ == "__main__":
     stress_test()
