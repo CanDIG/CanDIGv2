6808fdcbe2a1fe2241c41c2d7cbae9d166ff635e
fnguyen <OrdinaryNeumann@gmail.com>
2024-09-12 15:27:47 -0400

    Apply code comment suggestions by @daisieh

--- lib/katsu/katsu_setup.sh
+++ lib/katsu/katsu_setup.sh
@@ -7,16 +7,16 @@ LOGFILE=$PWD/tmp/progress.txt
 # This script runs after the container is composed.
 
 echo ">> waiting for katsu to start"
-ingest=$(docker ps --format "{{.Names}}" | grep katsu_1)
+katsu=$(docker ps --format "{{.Names}}" | grep katsu_1)
 while [ $? -ne 0 ]
 do
   echo "..."
   sleep 1
-  ingest=$(docker ps --format "{{.Names}}" | grep katsu_1)
+  katsu=$(docker ps --format "{{.Names}}" | grep katsu_1)
 done
 sleep 5
 
 
 bash $PWD/create_service_store.sh "katsu"
 
-docker restart $ingest
+docker restart $katsu
