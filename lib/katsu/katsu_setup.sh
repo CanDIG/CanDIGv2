e968fff8cbd9d49e280e7a35fef0b3ed78077109
fnguyen <OrdinaryNeumann@gmail.com>
2024-09-12 15:22:10 -0400

    Allow Katsu to use refresh tokens as well as access tokens

---
+++ lib/katsu/katsu_setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -Euo pipefail
+
+LOGFILE=$PWD/tmp/progress.txt
+
+# This script runs after the container is composed.
+
+echo ">> waiting for katsu to start"
+ingest=$(docker ps --format "{{.Names}}" | grep katsu_1)
+while [ $? -ne 0 ]
+do
+  echo "..."
+  sleep 1
+  ingest=$(docker ps --format "{{.Names}}" | grep katsu_1)
+done
+sleep 5
+
+
+bash $PWD/create_service_store.sh "katsu"
+
+docker restart $ingest
