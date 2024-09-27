9726673ac649d9c7345deab1604dece606a66bbb
Daisie Huang <daisieh@gmail.com>
2024-09-26 21:12:26 -0700

    need to disable approle before making a backup

--- lib/vault/make_backup.sh
+++ lib/vault/make_backup.sh
@@ -4,6 +4,7 @@ vault=$(docker ps -a --format "{{.Names}}" | grep vault_1 | awk '{print $1}')
 vault_runner=$(docker ps -a --format "{{.Names}}" | grep vault-runner_1 | awk '{print $1}')
 
 mkdir -p $(pwd)/tmp/vault/backup
+docker exec $vault vault auth disable approle/
 stop=$(docker stop $vault)
 zip=$(docker exec $vault_runner bash -c "cd /vault; tar -cz data/ > backup.tar.gz")
 copy=$(docker cp $vault_runner:/vault/backup.tar.gz $(pwd)/tmp/vault/backup/)
