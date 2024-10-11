b292a0e8660514fd33fb6826aeef07c915b7a748
Daisie Huang <daisieh@gmail.com>
2024-10-11 14:26:37 -0700

    use an env setting

--- lib/keycloak/credential_loader.sh
+++ lib/keycloak/credential_loader.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+if [ "$KEYCLOAK_PROXY_HEADERS" != "none" ]; then
+  export cli_settings="--proxy-headers=$KEYCLOAK_PROXY_HEADERS"
+fi
+
 # Load credentials from secrets
 export KEYCLOAK_ADMIN_PASSWORD=$(< /run/secrets/keycloak-admin-password)
 
 if [ "$CANDIG_PRODUCTION_MODE" = 1 ]; then
-  exec /opt/keycloak/bin/kc.sh --proxy-headers=xforwarded start
+  exec /opt/keycloak/bin/kc.sh start $cli_settings
 else
   exec /opt/keycloak/bin/kc.sh start-dev
 fi
