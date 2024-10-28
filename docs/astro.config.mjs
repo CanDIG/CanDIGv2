e676b37bf88f8f11b92e14dd6e5c7c58d82de66c
Marion <mshadbolt@users.noreply.github.com>
2024-10-28 12:13:00 -0700

    try starlight

---
+++ docs/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://candig.github.io',
+    base: 'CanDIGv2',
+});
