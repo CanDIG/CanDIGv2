a51a4d555707ae5ccd7ddc8a8ee7fd9ba9a7ace5
Marion <mshadbolt@users.noreply.github.com>
2024-11-11 11:21:34 -0800

    play with header

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,114 +5,120 @@ import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi'
 
 import icon from 'astro-icon';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://candig.github.io',
     base: 'CanDIGv2',
-    integrations: [
-        icon({
-            include: {
-                mdi: ["*"]
-            }
-        }),
-        starlight({
-        title: 'Docs',
-        customCss: [
-            './src/styles/custom.css'
-        ],
-        favicon: '/favicon.ico',
-        editLink: {
-            baseUrl: 'https://github.com/CanDIG/CanDIGv2/edit/develop/'
-        },
-        logo: {
-            src: './src/assets/my-logo.png',
-            replacesTitle: true,
-        },
-        social: {
-            github: 'https://github.com/candig/CanDIGv2',
-        },
-        plugins: [
-            starlightOpenAPI([
-                {
-                    base: 'technical/ingest',
-                    label: 'ingest api',
-                    schema: 'https://raw.githubusercontent.com/CanDIG/candigv2-ingest/refs/heads/mshadbolt/improve-openapi-schema/ingest_openapi.yaml',
-                    collapsed: true
-                },
-                {
-                    base: 'technical/query',
-                    label: 'query api',
-                    schema: 'https://raw.githubusercontent.com/CanDIG/candigv2-query/refs/heads/stable/query_server/openapi.yaml',
-                    collapsed: true
-                },
-                {
-                    base: 'technical/katsu',
-                    label: 'katsu api',
-                    schema: 'https://raw.githubusercontent.com/CanDIG/katsu/refs/heads/stable/chord_metadata_service/mohpackets/docs/schemas/schema.yml',
-                    collapsed: true
-                },
-                {
-                    base: 'technical/htsget/drs',
-                    label: 'htsget drs api',
-                    schema: 'https://raw.githubusercontent.com/CanDIG/htsget_app/refs/heads/stable/htsget_server/drs_openapi.yaml',
-                    collapsed: true
-                },
-                {
-                    base: 'technical/htsget/beacon',
-                    label: 'htsget beacon api',
-                    schema: 'https://raw.githubusercontent.com/CanDIG/htsget_app/refs/heads/stable/htsget_server/beacon_openapi.yaml',
-                    collapsed: true
-                },
-                {
-                    base: 'technical/htsget/operations',
-                    label: 'htsget operations api',
-                    schema: 'https://raw.githubusercontent.com/CanDIG/htsget_app/refs/heads/stable/htsget_server/htsget_openapi.yaml',
-                    collapsed: true
-                },
-            ])
-        ],
-        sidebar: [
+    integrations: [icon({
+        include: {
+            mdi: ["*"]
+        }
+    }), starlight({
+    title: 'Docs',
+    components: {
+        Header: "./src/components/Header.astro",
+        ThemeSelect: "./src/components/ThemeSelect.astro",
+        Select: "./src/components/Select.astro",
+    },
+    customCss: [
+        './src/styles/custom.css'
+    ],
+    favicon: '/favicon.ico',
+    editLink: {
+        baseUrl: 'https://github.com/CanDIG/CanDIGv2/edit/develop/'
+    },
+    logo: {
+        src: './src/assets/my-logo.png',
+        replacesTitle: true,
+    },
+    social: {
+        github: 'https://github.com/candig/CanDIGv2',
+    },
+    plugins: [
+        starlightOpenAPI([
             {
-                label: 'Deployment',
-                items: [
-                    { label: 'Local deployment', slug: 'deployment/local' },
-                    { label: 'Production deployment', slug: 'deployment/production'},
-                    { label: 'Testing', slug: 'deployment/ingest-and-test'},
-                    { label: 'Interact using Make', slug: 'deployment/interact-with-the-stack'},
-                    { label: 'User roles', slug: 'deployment/user-roles'},
-                    { label: 'Logging', slug: 'deployment/logging'},
-                    { label: 'Back up/Restore', slug: 'deployment/backup-restore-candig'},
-                    { label: 'Troubleshooting', slug: 'deployment/stack-troubleshooting'}, 
-                ]
+                base: 'technical/ingest',
+                label: 'ingest api',
+                schema: 'https://raw.githubusercontent.com/CanDIG/candigv2-ingest/refs/heads/mshadbolt/improve-openapi-schema/ingest_openapi.yaml',
+                collapsed: true
+            },
+            {
+                base: 'technical/query',
+                label: 'query api',
+                schema: 'https://raw.githubusercontent.com/CanDIG/candigv2-query/refs/heads/stable/query_server/openapi.yaml',
+                collapsed: true
+            },
+            {
+                base: 'technical/katsu',
+                label: 'katsu api',
+                schema: 'https://raw.githubusercontent.com/CanDIG/katsu/refs/heads/stable/chord_metadata_service/mohpackets/docs/schemas/schema.yml',
+                collapsed: true
+            },
+            {
+                base: 'technical/htsget/drs',
+                label: 'htsget drs api',
+                schema: 'https://raw.githubusercontent.com/CanDIG/htsget_app/refs/heads/stable/htsget_server/drs_openapi.yaml',
+                collapsed: true
+            },
+            {
+                base: 'technical/htsget/beacon',
+                label: 'htsget beacon api',
+                schema: 'https://raw.githubusercontent.com/CanDIG/htsget_app/refs/heads/stable/htsget_server/beacon_openapi.yaml',
+                collapsed: true
+            },
+            {
+                base: 'technical/htsget/operations',
+                label: 'htsget operations api',
+                schema: 'https://raw.githubusercontent.com/CanDIG/htsget_app/refs/heads/stable/htsget_server/htsget_openapi.yaml',
+                collapsed: true
+            },
+        ])
+    ],
+    sidebar: [
+        {   
+            collapsed: true,
+            label: 'Deployment',
+            items: [
+                { label: 'Local deployment', slug: 'deployment/local' },
+                { label: 'Production deployment', slug: 'deployment/production'},
+                { label: 'Testing', slug: 'deployment/ingest-and-test'},
+                { label: 'Interact using Make', slug: 'deployment/interact-with-the-stack'},
+                { label: 'User roles', slug: 'deployment/user-roles'},
+                { label: 'Logging', slug: 'deployment/logging'},
+                { label: 'Back up/Restore', slug: 'deployment/backup-restore-candig'},
+                { label: 'Troubleshooting', slug: 'deployment/stack-troubleshooting'}, 
+            ]
 
-            },
-            {
-                label: 'Guides',
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { 
-                        label: 'Data ingest steps', 
-                        items: 
-                        [
-                            'guides/ingest/prepare-clinical', 
-                            'guides/ingest/register-programs', 
-                            'guides/ingest/ingest-clinical',
-                            'guides/ingest/prepare-genomic',
-                            'guides/ingest/ingest-genomic',
-                            'guides/ingest/ingest-help',
-                        ]
-                    },
-                    { label: 'Data Exploration', slug: 'guides/explore' },
-                ],
-            },
-            {
-                label: 'Technical Docs',
-                items: [ 
-                    { label: 'Architecture', slug: 'technical/architecture' },
-                    { label: 'Docker and submods', slug: 'technical/docker-and-submodules' },
-                    ...openAPISidebarGroups,
-                ]
-            },
-        ],
-		}), icon()],
+        },
+        {
+            label: 'Guides',
+            items: [
+                // Each item here is one entry in the navigation menu.
+                { 
+                    label: 'Data ingest steps', 
+                    items: 
+                    [
+                        'guides/ingest/prepare-clinical', 
+                        'guides/ingest/register-programs', 
+                        'guides/ingest/ingest-clinical',
+                        'guides/ingest/prepare-genomic',
+                        'guides/ingest/ingest-genomic',
+                        'guides/ingest/ingest-help',
+                    ]
+                },
+                { label: 'Data Exploration', slug: 'guides/explore' },
+            ],
+        },
+        {
+            label: 'Technical Docs',
+            items: [ 
+                { label: 'Architecture', slug: 'technical/architecture' },
+                { label: 'Docker and submods', slug: 'technical/docker-and-submodules' },
+                ...openAPISidebarGroups,
+            ]
+        },
+    ],
+		}), tailwind()]
 });
