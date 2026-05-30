47ae475739ad9c3c74ece8a4770f2e2af7f0105f
Daisie Huang <daisieh@gmail.com>
2026-05-29 17:47:27 -0700

    use as_self param when initializing our server

--- lib/federation/initialize.py
+++ lib/federation/initialize.py
@@ -55,7 +55,7 @@ def main():
     server = get_default_server()
     if json.dumps(server['server'], sort_keys=True) not in json.dumps(response.json(), sort_keys=True):
         print("Adding our server to the federation")
-        requests.request("POST", url, headers=headers, json=server)
+        requests.request("POST", url, headers=headers, json=server, params={"as_self": True})
         response = requests.request("GET", url, headers=headers)
     print(response.text)
 
