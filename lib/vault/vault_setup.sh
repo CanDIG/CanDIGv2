dbf6273bea9ead6f054c30d5ea25893974de7a29
Daisie Huang <daisieh@gmail.com>
2026-06-11 21:16:10 -0700

    actually just install the small restore file, it's fine

--- lib/vault/vault_setup.sh
+++ lib/vault/vault_setup.sh
@@ -45,15 +45,6 @@ if [[ -f "lib/vault/restore.tar.gz" ]]; then
   if [[ $(($size)) < 50000 ]]; then
     echo -e "🚨🚨🚨 ${RED}BAD RESTORE FILE${DEFAULT} 🚨🚨🚨"
     echo "The backup you are restoring from is less than 50kb in size, which is suspiciously small."
-    echo "Creating new vault from scratch."
-    pwd=$(pwd)
-    cd lib/vault/tmp
-    tar -xzf $pwd/lib/vault/restore.tar.gz
-    cd $pwd
-    cp lib/vault/tmp/backup/service_stores.txt tmp/vault/
-    rm -R lib/vault/tmp/backup
-    mv lib/vault/restore.tar.gz lib/vault/not_restored.tar.gz
-  else
     docker stop $vault
     pwd=$(pwd)
     cd lib/vault/tmp
